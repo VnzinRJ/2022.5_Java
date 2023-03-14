eca812362cad7f0a4d6161988b7b0855f76a28ae
Técnico em Informática - 2022.5 <marcosvinicius0733@gmail.com>
2023-03-13 21:46:14 -0300

    aula02

--- src/basejava/Numero.java
+++ src/basejava/Numero.java
@@ -4,11 +4,17 @@ package basejava;
 public class Numero {
     int valor;
     
-    public String parInpar(){
-    if(this.valor % 2==0){
+    public String parImpar(){
+    if(this.valor % 2 ==0){
            return "PAR";
        }else{
            return "IMPAR";
        }
+    }
+    public int maiorValor(int valor1, int valor2){
+        if(valor1 > valor2){
+            return valor1;
+        }
+        return valor2;
     }
 }
--- src/basejava/Principal.java
+++ src/basejava/Principal.java
@@ -10,18 +10,21 @@ public class Principal {
 
     
     public static void main(String[] args) {
-//       int valor = 3;
-//       
-//       if(valor % 2 == 0){
-//           System.out.print("PAR");
-//       }else{
-//           System.err.print("IMPAR");
-//       };
+       int valor = 3;
+      
+     if(valor % 2 == 0){
+          System.out.print("PAR");
+      }else{
+          System.err.print("IMPAR");
+      };
         
           Numero numero = new Numero();
           numero.valor =4;
-          System.out.print(numero.parInpar());
-    }
+          System.out.print(numero.parImpar());
+   
+        System.out.print(numero.maiorValor(valor1:6 , valor2:8));
+   
+        }
     }
     
 
